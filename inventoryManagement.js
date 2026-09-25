591ad183732a942e30e869868b39e7f4aba7fcee
Ashley tracy <@tashatracy26@gmail.com>
2026-09-25 12:19:22 +0300

    Add addProduct function

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,6 +1,22 @@
 // Write your code here
+let products = ["Laptop", "Phone", "Headphones", "Monitor"];
 
+function logFirstProduct() {
+  console.log(products[0]);
+}
 
+function addProduct(productName) {
+  products.push(productName);
+}
+
+function updateProductName(position, newName) {
+  products[position] = newName;
+}
+
+function removeLastProduct() {
+  products.pop();
+  return products;
+}
 
 // Export the necessary parts for testing
 module.exports = {
